0edd52fcc7288de38285cd1905c219b8cfb3c5ef
Mohammad ZainulAbideen <mdzayan572@gmail.com>
2023-08-26 11:03:04 +0530

    first commit

--- config/UserModel.js
+++
@@ -1,8 +0,0 @@
-const mongoose=require('mongoose')
-const schema=new mongoose.Schema({
-    username:String,
-    password:String,
-    email:{type:String}
-})
-const model=new mongoose.model('credentials',schema)
-module.exports=model;
